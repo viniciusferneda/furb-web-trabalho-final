567a8593247222480e6570adb3e5fa91c716e7e6
Vinicius <Vinicius@Vinicius-PC>
2013-12-03 23:58:42 -0200

    cadastro de avaliação

--- certics/JavaSource/br/certics/view/bean/avaliacao/AvaliacaoBean.java
+++ certics/JavaSource/br/certics/view/bean/avaliacao/AvaliacaoBean.java
@@ -9,6 +9,7 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
 import br.certics.controller.facade.AvaliacaoFacade;
+import br.certics.controller.facade.PerguntaRespostaFacade;
 import br.certics.controller.facade.PessoaFisicaFacade;
 import br.certics.controller.facade.ResultadoEsperadoFacade;
 import br.certics.controller.facade.SoftwareFacade;
@@ -17,6 +18,8 @@ import br.certics.model.entity.PerguntaRespostaEntity;
 import br.certics.model.entity.PessoaFisicaEntity;
 import br.certics.model.entity.ResultadoEsperadoEntity;
 import br.certics.model.entity.SoftwareEntity;
+import br.certics.model.enums.EscalaPontuacaoAva;
+import br.certics.model.enums.EscalaPontuacaoRE;
 import br.certics.view.bean.ApplicationContextBean;
 import br.certics.view.bean.MessageUtils;
 import br.finf.control.facade.FacadeProvider;
@@ -30,7 +33,7 @@ public class AvaliacaoBean {
 	private PessoaFisicaEntity avaliador = new PessoaFisicaEntity();
 	
 	private List<PerguntaRespostaEntity> lPerguntaResposta = new ArrayList<PerguntaRespostaEntity>();
-
+	
 	@ManagedProperty(value="#{applicationContextBean}")
 	private ApplicationContextBean applicationContext;
 	
@@ -43,15 +46,40 @@ public class AvaliacaoBean {
 		SoftwareFacade softwareFacade = FacadeProvider.get().provide(SoftwareFacade.class);
 		return softwareFacade.selectAll();
 	}
+	
+	public List<EscalaPontuacaoRE> getAllEscalaPontuacaoRE(){
+		EscalaPontuacaoRE[] escalaPontuacaoREs = EscalaPontuacaoRE.values();
+		List<EscalaPontuacaoRE> lEscalaPontuacaoRE = new ArrayList<EscalaPontuacaoRE>();
+		for (int i = 0; i < escalaPontuacaoREs.length; i++) {
+			lEscalaPontuacaoRE.add(escalaPontuacaoREs[i]);
+		}
+		return lEscalaPontuacaoRE;
+	}
 
 	public void salvar() {
 		AvaliacaoFacade facade = FacadeProvider.get().provide(AvaliacaoFacade.class);
 		avaliacao.setSoftware(software);
 		avaliacao.setAvaliador(avaliador);
 		avaliacao.setPerguntaResposta(lPerguntaResposta);
+		avaliacao.setEscalaPontuacaoAva(EscalaPontuacaoAva.S);
+		for (PerguntaRespostaEntity pergRes : lPerguntaResposta) {
+			if(EscalaPontuacaoRE.N.equals(pergRes.getEscalaPontuacaoRE()) || EscalaPontuacaoRE.P.equals(pergRes.getEscalaPontuacaoRE())){
+				avaliacao.setEscalaPontuacaoAva(EscalaPontuacaoAva.N);
+				break;
+			}
+		}
 		facade.salvar(avaliacao);
+		salvarPerguntaResposta();
 		MessageUtils.addInfoMessage("Avaliação salva com sucesso!");
 		limpar();
+	}
+
+	private void salvarPerguntaResposta() {
+		PerguntaRespostaFacade perguntaRespostaFacade = FacadeProvider.get().provide(PerguntaRespostaFacade.class);
+		for (PerguntaRespostaEntity pergRes : lPerguntaResposta) {
+			pergRes.setAvaliacao(avaliacao);
+			perguntaRespostaFacade.salvar(pergRes);
+		}
 	}
 
 	@PostConstruct
@@ -67,6 +95,7 @@ public class AvaliacaoBean {
 	}
 
 	public void criaListaPerguntaResposta(){
+		lPerguntaResposta = new ArrayList<PerguntaRespostaEntity>();
 		ResultadoEsperadoFacade resultadoEsperadoFacade = FacadeProvider.get().provide(ResultadoEsperadoFacade.class);
 		List<ResultadoEsperadoEntity> lResultadoEsperado = resultadoEsperadoFacade.selectAll();
 		for (ResultadoEsperadoEntity resultadoEsperado : lResultadoEsperado) {
@@ -76,7 +105,7 @@ public class AvaliacaoBean {
 			lPerguntaResposta.add(perguntaResposta);
 		}
 	}
-
+	
 	public ApplicationContextBean getApplicationContext() {
 		return applicationContext;
 	}
